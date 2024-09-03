3265efef6a3d29f9149d35e45ce25d81c41e66a7
wawan <wawanadesaputra17@gmail.com>
2024-09-03 22:09:07 +0700

    add Varvsval.kt

---
+++ src/main/kotlin/VarvsVal.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+    var varVariabel: Int
+    varVariabel = 56 //Assigning first value to varVariabel
+    println("Pemberian nilai pertama pada varVariabel = " + varVariabel)
+
+    varVariabel = 78 //Assigning second value to varVariabel
+    println("Pemberian nilai kedua pada varVariabel = " + varVariabel)
+}

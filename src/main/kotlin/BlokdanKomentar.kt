f457e1b2a08ddf78fc9fc19fe2c32c3f845feca4
wawan <wawanadesaputra17@gmail.com>
2024-09-03 21:31:45 +0700

    add BlokdanKomentar.kt

---
+++ src/main/kotlin/BlokdanKomentar.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) {
+    println("Hello, ini program pertama saya di Kotlin!")
+
+    //Contoh komentar satu baris
+
+    /*
+    Contoh komentar multi baris
+    */
+
+    /**
+     * Kotlin Doc --> Baris ini untuk mendokumentasikan kode program
+     */
+}

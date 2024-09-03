8f0ac399246325cfa02ebbce0c0cc1918a1bec5c
wawan <wawanadesaputra17@gmail.com>
2024-09-03 21:24:47 +0700

    add Operator.kt

---
+++ src/main/kotlin/Operator.kt
@@ -0,0 +1,19 @@
+fun main(args: Array<String>) {
+    var ArithmeticOperator = "+ , - , * , / , %";
+    var EqualSymbol = "="
+    var AssignmentOperator = "+= , -= , *= , %="
+    var LogicalOperator = "&& , || , ! , 'and' , 'or' , 'not'"
+    var EqualityOperator = "== , !="
+    var ReferentialOperator = "=== , !=="
+    var ComparisonOperator = "< , > , <= , >="
+    var IndexAccesOperator = "[] , [,]"
+
+    println("ArithmeticOperator = " + ArithmeticOperator)
+    println("EqualSymbol = " + EqualSymbol)
+    println("AssignmentOperator = " + AssignmentOperator)
+    println("LogicalOperator = " + LogicalOperator)
+    println("EqualityOperator = " + EqualityOperator)
+    println("ReferentialOperator = " + ReferentialOperator)
+    println("ComparisonOperator = " + ComparisonOperator)
+    println("IndexAccesOperator = " + IndexAccesOperator)
+}
